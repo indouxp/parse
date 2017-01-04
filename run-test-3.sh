5f32473cc15a13adf50a61ce0f66eca992e640c5
oracle <oracle@std.tsystem.gr.jp>
2017-01-04 22:42:59 +0900

    add run-test-[23].sh

--- run-test-3.sh
+++ run-test-3.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
+BASE=${0##*/}
+BASE=${BASE%.*}
+LOG=${0##*/}.log
 
-cat <<EOT > run-test-2.1.sql
+cat <<EOT > ./sql.d/${BASE:?}.1.sql
+set echo on
 -- バッファ・キャッシュと共有プールにキャッシュされている情報をクリア
 alter system flush shared_pool ;
 exit 0
 EOT
 
-sqlplus / as sysdba @ ./run-test-2.1.sql
+sqlplus / as sysdba @ ./sql.d/${BASE:?}.1.sql | tee ${LOG:?}
 
 # リテラルのSELECT文を10000回連続実行
 time ( date ; for i in `seq 1 10000`; do
@@ -15,10 +19,11 @@ time ( date ; for i in `seq 1 10000`; do
                 echo "select * from TAB36 where COL1=:B1 ;"
               done | sqlplus TRY/TRY > /dev/null ; date )
 
-cat <<EOT > run-test-2.2.sql
+cat <<EOT > ./sql.d/${BASE:?}.2.sql
+set echo on
 -- 実行計画が作成されたSQLの合計数
 select count(*) from V\$SQL where SQL_TEXT like 'select * from TAB36 where COL1=%' ;
 exit 0
 EOT
 
-sqlplus / as sysdba @ ./run-test-2.2.sql
+sqlplus / as sysdba @ ./sql.d/${BASE:?}.2.sql | tee -a ${LOG:?}
